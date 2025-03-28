75c0c885011bbe0b592673901b40f6543ef4fdb8
Max <cs.goicemax@gmail.com>
2025-03-28 20:22:10 +0700

    #

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ b = "ABCDEFGHJIKLMNOPQRSTUVWXYZ"
 c = "0123456789"
 d = "[]{}()/,.?|;:'!@#$%^&*_-=+"
 
-length = int(input("Enter the length of the password: "))
+length = input("Enter the length of the password: ")
 ali = a + b + c + d
 password = "".join(random.sample(ali, length))
 print("Generate password ...")
